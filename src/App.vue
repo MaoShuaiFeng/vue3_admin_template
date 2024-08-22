645409696ad23f363ccdc66e73c993d96da2a821
Maoshuaifeng <maoshuaifeng@tecpie.com>
2024-08-23 02:33:33 +0800

    config:init

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
     <h1>App</h1>
+    <el-button :icon="Plus">123</el-button>
+    <el-button type="primary" :icon="Edit">123</el-button>
+    <el-button type="success" size="large" :icon="Edit">123</el-button>
+    <el-pagination
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000"
+    />
   </div>
+  <A></A>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Edit, Plus } from "@element-plus/icons-vue";
+</script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+// main.ts
+import { createApp } from "vue"; // 从vue中导入createApp
+import ElementPlus from "element-plus"; // 从element-plus中导入ElementPlus
+import "element-plus/dist/index.css"; // 导入element-plus的css样式
+import App from "./App.vue"; // 从App.vue中导入App
+import zhCn from "element-plus/es/locale/lang/zh-cn";
+const app = createApp(App); // 使用createApp创建一个vue实例
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.mount("#app"); // 将vue实例挂载到id为app的元素上
