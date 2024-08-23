9103f50851e950f56d820549f7ee93be22673ff7
Maoshuaifeng <maoshuaifeng@tecpie.com>
2024-08-24 02:40:34 +0800

    fix:配置vite别名，更改ts的config文件，可以鼠标点击到文件原来的位置

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import { constantRoutes } from "./routes";
 //创建路由器
-let router = createRouter({
+const router = createRouter({
   history: createWebHashHistory(),
   routes: constantRoutes,
   //滚动行为
