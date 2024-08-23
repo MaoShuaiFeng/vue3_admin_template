11ecb5fb2f29a1606013d4c7d4bf31b39243eb24
Maoshuaifeng <maoshuaifeng@tecpie.com>
2024-08-24 04:56:28 +0800

    feat:添加左侧菜单组件

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,19 +2,35 @@
   <div class="layout_container">
     <!-- 左侧菜单 -->
     <div class="layout_slider">
-        <Logo></Logo>
+      <Logo></Logo>
+      <!-- 展示菜单 -->
+      <!-- 滚动组件 -->
+      <el-scrollbar class="scrollbar">
+        <!-- 菜单组件 -->
+        <el-menu>
+          <el-menu-item index="1">首页</el-menu-item>
+          <el-menu-item index="2">数据大屏</el-menu-item>
+          <!-- 折叠菜单 -->
+          <el-sub-menu index="3">
+            <template #title>权限管理</template>
+            <el-menu-item index="3-1">用户管理</el-menu-item>
+            <el-menu-item index="3-2">角色管理</el-menu-item>
+            <el-menu-item index="3-3">菜单管理</el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-scrollbar>
     </div>
     <!-- 顶部菜单 -->
     <div class="layout_tabbar">2</div>
     <!-- 内容展示区 -->
     <div class="layout_main">
-        <p style="height: 10000px;background-color: red;">123</p>
+      <p style="height: 10000px; background-color: red">123</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import Logo from "@/layout/logo/index.vue"
+import Logo from "@/layout/logo/index.vue";
 </script>
 
 <style scoped lang="scss">
@@ -23,10 +39,26 @@ import Logo from "@/layout/logo/index.vue"
   height: 100vh;
   background-color: red;
 
+  & {
+    --el-menu-text-color: rgb(188, 188, 230);
+    --el-menu-bg-color: rgb(62, 62, 119);
+    --el-menu-hover-bg-color: rgb(80, 89, 219);
+    --el-menu-active-color: white;
+  }
+
+  .el-menu-item.is-active {
+    background-color: #247bb4;
+  }
+
   .layout_slider {
     width: $base-menu-width;
     height: 100vh;
     background-color: $base-menu-background;
+
+    .scrollbar {
+      width: 100%;
+      height: calc(100vh - $base-menu-logo-height);
+    }
   }
   .layout_tabbar {
     position: fixed;
