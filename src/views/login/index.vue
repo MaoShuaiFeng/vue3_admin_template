3c6e936413c72201f6bc22ea945e2d18c6d8d6d5
Maoshuaifeng <maoshuaifeng@tecpie.com>
2024-08-23 23:00:46 +0800

    feat:静态首页搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,44 @@
 <template>
   <div class="login_container">
     <el-row>
-        <el-col :span="12" :xs="0">左</el-col>
-        <el-col :span="12" :xs="24">油</el-col>
+      <el-col :span="12" :xs="0"></el-col>
+      <el-col :span="12" :xs="24">
+        <el-form class="login_form">
+          <h1>Hello</h1>
+          <h2>welcome</h2>
+          <el-form-item>
+            <el-input
+              v-model="loginForm.username"
+              :prefix-icon="User"
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input
+              v-model="loginForm.password"
+              type="password"
+              :prefix-icon="Lock"
+              show-password
+            ></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button class="login_btn" type="primary" size="default"
+              >登录</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { User, Lock } from "@element-plus/icons-vue";
+import { reactive } from "vue";
+//收集账号密码数据
+let loginForm = reactive({
+  username: "admin",
+  password: "111111",
+});
 </script>
 
 <style scoped lang="scss">
@@ -17,5 +47,29 @@
   height: 100vh;
   background: url("@/assets/images/background.jpg") no-repeat;
   background-size: cover;
+}
+
+.login_form {
+  position: relative;
+  width: 80%;
+  top: 30vh;
+  background: url("@/assets/images/background.jpg") no-repeat;
+  background-size: cover;
+  padding: 40px;
+
+  h1 {
+    color: white;
+    font-size: 40px;
+  }
+
+  h2 {
+    color: white;
+    font-size: 20px;
+    margin: 20px 0;
+  }
+
+  .login_btn {
+    width: 100%;
+  }
 }
 </style>
