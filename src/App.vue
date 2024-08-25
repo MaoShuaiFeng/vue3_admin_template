0e0a1f885e01be5a85441b78831ff2b57e9b9085
Maoshuaifeng <maoshuaifeng@tecpie.com>
2024-08-26 03:51:29 +0800

    style:路由组件切换添加动画

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <div>
-    <router-view></router-view>
+    <!-- <router-view></router-view> -->
+    <!-- 路由组件出口位置 -->
+    <router-view v-slot="{ Component }">
+      <transition name="fade">
+        <!-- 渲染layout以及路由的子路由 -->
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-from {
+  opacity: 0;
+  transform: scale(0.5);
+}
+
+.fade-enter-active {
+  transition: all 0.1s ease;
+}
+
+.fade-enter-to {
+  opacity: 1;
+  transform: scale(1);
+}
+</style>
