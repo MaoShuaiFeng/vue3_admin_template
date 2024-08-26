5e959c69165487af14bd96e181cfdeed073f2be1
Maoshuaifeng <maoshuaifeng@tecpie.com>
2024-08-27 04:57:33 +0800

    config:vite配置代理

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
@@ -6,7 +6,9 @@ import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import { viteMockServe } from "vite-plugin-mock";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  // 获取环境变量
+  const env = loadEnv(mode, process.cwd());
   return {
     plugins: [
       vue(),
@@ -31,6 +33,15 @@ export default defineConfig(({ command }) => {
         scss: {
           javascriptEnabled: true,
           additionalData: `@import "@/style/variable.scss";`,
+        },
+      },
+    },
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          target: env.VITE_SERVER,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
     },
