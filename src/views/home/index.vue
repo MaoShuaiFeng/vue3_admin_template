f6f8dc33268cf3245fb893d7e66e3f05e8505136
maoshuaifeng <393154860@qq.com>
2024-09-05 05:37:40 +0800

    feat:样式调整和首页组件重构
    - 移除reset.scss中不再使用的字体继承和大小重置。
    - 重构首页(index.vue)，用el-card替代基础div布局，增强模块化和视觉效果。
    - 实现用户信息的组件化展示，使用flex布局优化元素对齐。
    - 通过引入userStore，实现用户头像和用户名的动态显示。
    - 样式定义中采用scss语法增强可读性和维护性。

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,46 @@
 <template>
   <div>
-    <h2>home</h2>
-    <div style="width: 100%;background-color: red; height: 100px">123</div>
+    <el-card >
+      <div class="box">
+        <img :src="userStore.avatar" alt="用户头像"></img>
+        <div>
+          <p>
+            {{ userStore.username }}
+          </p>
+          <span>后台管理系统</span>
+        </div>
+      </div>
+    </el-card>
   </div>
 </template>
 
 <script setup lang="ts">
-
+import useUserStore from "@/store/modules/user";
+let userStore = useUserStore();
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.box {
+  display: flex;
+  img{
+    width: 180px;
+    height: 180px;
+    border-radius: 50%;
+    margin-right: 20px;
+  }
+  div{
+ p{
+  font-size: 40px;
+  font-weight: 700;
+  margin: 30px
+ }
+ span{
+  font-size: 20px;
+  font-weight: 600;
+  margin-left: 30px;
+  color: rgb(175, 175, 175);
+  font-style: italic;
+ }
+  }
+}
+</style>
