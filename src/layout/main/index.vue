e8fdc57a8b24026431a6db26fab7a2794b8e3aea
maoshuaifeng <393154860@qq.com>
2024-09-10 16:18:08 +0800

    feat:优化动画效果，实现路由动态过渡，调整样式增强用户体验

--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- 路由组件出口位置 -->
   <router-view v-slot="{ Component }">
-    <transition name="fade">
+    <transition name="fade" appear mode="out-in" position="absolute">
       <!-- 渲染layout以及路由的子路由 -->
       <component :is="Component" v-if="isDestroy" />
     </transition>
@@ -35,17 +35,14 @@ export default {
 </script>
 
 <style scoped>
-.fade-enter-from {
+.fade-enter-from,
+.fade-leave-to {
   opacity: 0;
-  transform: scale(0);
+  transform: translateX(20px);
 }
 
-.fade-enter-active {
+.fade-enter-active,
+.fade-leave-active {
   transition: all 0.3s ease;
-}
-
-.fade-enter-to {
-  opacity: 1;
-  transform: scale(1);
 }
 </style>
