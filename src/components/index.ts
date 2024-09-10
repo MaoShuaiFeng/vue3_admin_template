5e0538188d6d5958cc8dbb45653c0eecfe7b25d1
maoshuaifeng <393154860@qq.com>
2024-09-11 02:32:21 +0800

    feat:移除SvgIcon组件，更新components引入，优化代码结构

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,9 @@
 import type { App, Component } from "vue";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue"; // 引入element-plus图标
-import SvgIcon from "./SvgIcon/index.vue";
+// import SvgIcon from "./SvgIcon/index.vue";
 import Category from "./Category/index.vue";
 
-const components: { [name: string]: Component } = { SvgIcon, Category };
+const components: { [name: string]: Component } = { Category };
 
 export default {
   install(app: App) {
